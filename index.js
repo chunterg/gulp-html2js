16ec25dff77d1e2d7cb00ef1c81c94d7c0d5902f
chunterg <{geng.cheng@alibaba-inc.com}>
2014-04-13 18:29:12 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var gutil = require('gulp-util');
+module.exports = function(opt){
+	var options = opt||{
+		encode:'gbk'
+	};
+
+	function trans(file,cb){
+		if (file.isNull()) {
+	      return cb(null, file);
+	    }
+
+	    if (file.isStream()) {
+	      return cb(new gutil.PluginError('gulp-html2js', 'Streaming not supported'));
+	    }
+
+	    var fileContent = String(file.contents)
+	}
+
+	return trans;
+}
